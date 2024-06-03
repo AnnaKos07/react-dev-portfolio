de2078fb0e9d9788ebaa724c33eafe968669ea64
AnnaKos07 <annakos070500@gmail.com>
2024-06-03 20:35:50 +0200

    new_avatar

--- src/content/information.js
+++ src/content/information.js
@@ -4,7 +4,7 @@ const userData = {
   firstName: "Hanna",
   lastName: " Kastrytsa",
   title: "About Me",
-  img: "https://images.ctfassets.net/3i4n4r7kp8f6/1oeDjOsXqvlXtenb2FRr3H/5717ef0d0b2ccea89d0ed397685f4545/profile_photo.png",
+  img: "https://images.ctfassets.net/3i4n4r7kp8f6/1oeDjOsXqvlXtenb2FRr3H/e4706ab3b07b60a3d2d8e86b359c8792/avatar.png",
   description:
     "Welcome to my portfolio, which showcases my expertise in product design. I have a keen eye for detail and a commitment to innovation, specialising in creating compelling and functional product experiences. ",
 };
