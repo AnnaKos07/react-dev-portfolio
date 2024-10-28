f0081715e9223815f138d57d69305c41de6b2806
AnnaKos07 <annakos070500@gmail.com>
2024-10-28 14:45:53 +0100

    info upd 2

--- src/content/information.js
+++ src/content/information.js
@@ -6,7 +6,7 @@ const userData = {
   title: "About Me",
   img: "https://images.ctfassets.net/3i4n4r7kp8f6/1oeDjOsXqvlXtenb2FRr3H/994d910245823b520213df74f62b2569/portrait.png",
   description:
-    "UX Researcher and UI Designer with 4 years in UX research, UI service design, high-fidelity prototyping to create healthcare solutions. Developed the marketplace and CRM system from scratch for HealthTech startup in Israel with 50+ B2B clients. Conducted the UX research and prototyped the Voice assistant and multimodal interactions for the device preventing UTI diseases among young women. Currently finishing my Master of Arts in Interaction Design at SUPSI in Switzerland. Former Frontend Developer, proficient in 3D modeling.",
+    "UX Researcher and UI Designer with 4 years in UX research, UI service design, high-fidelity prototyping to create healthcare solutions. Developed the marketplace and CRM system from scratch for HealthTech startup in Israel with 50+ B2B clients. Currently finishing my Master of Arts in Interaction Design at SUPSI in Switzerland. Former Frontend Developer, proficient in 3D modeling.",
 };
 
 //personal information end
